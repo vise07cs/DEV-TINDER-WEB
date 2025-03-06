3b24a2e6e00002dc3d64cd44d06c8d19cd59c11e
vise07cs <vikram7cs@gmail.com>
2025-03-06 13:49:14 +0530

    configuring DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+// import "./index.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  //   <h1 class="text-3xl font-bold underline">
+  //   Hello world!
+  // </h1>
+  <div className="navbar bg-base-200 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">DEVinder</a>
+  </div>
+  <div className="flex gap-2">
+    {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
+    <div className="dropdown dropdown-end mx-8">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
+
 
 
 
